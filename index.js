d0e7df2e4d128376edf1699dfb6bfc7da835b294
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2022-04-24 18:58:20 -0300

    feat: add functions

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const checkIfEndStr = str => str.endsWith('srt')
 const pathFile = filename => path.resolve(__dirname, "legendas", filename)
 const joinPathFile = file => fs.readFileSync(file, "utf8")
 const splitString = str => word => word.split(str)
+const graterThanZero = arr => arr.length > 0
+const removeSpaces = str => str.trim()
+const sortByNum = (a, b) => b.num - a.num
 
 const splitForLine = splitString("\n")
 const pathFolder = takePath("legendas")
@@ -16,7 +19,7 @@ const files = allFiles.filter(checkIfEndStr)
 
 const texto = files.map(fileName => joinPathFile(pathFile(fileName)))
 const array = texto.map(splitForLine).flat()
-const arrayWithOutSpace = array.map(word => word.trim())
+const arrayWithOutSpace = array.map(removeSpaces)
 const textWithCaracteres = arrayWithOutSpace.join(",")
 
 const charForReplace = caracteres.join("|")
@@ -24,7 +27,7 @@ const charForReplace = caracteres.join("|")
 const textWithCaracteresReplace = textWithCaracteres.replace(new RegExp(`[${charForReplace}]`, "g"), '')
 const txt = textWithCaracteresReplace.replace(/[0-9]/g, '').replace(/\[|]|-|=|,|&|\$/g, ' ')
 const txtInLines = txt.split(" ").join("\n")
-const words = txtInLines.split("\n").filter(word => word.length > 0)
+const words = txtInLines.split("\n").filter(graterThanZero)
 
 countWords = []
 words.map((word) => {
@@ -38,14 +41,5 @@ words.map((word) => {
   index !== -1 ? countWords[index].num += 1 : countWords.push(wd)
 })
 
-const arrayTotalWords = countWords.sort(function (a, b) {
-  if (a.num < b.num) {
-    return 1;
-  }
-  if (a.num > b.num) {
-    return -1;
-  }
-  return 0;
-})
-console.log("-> ~ arrayTotalWords", arrayTotalWords)
+countWords.sort(sortByNum)
 
